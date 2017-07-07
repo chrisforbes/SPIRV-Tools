36ea38eef982631ec2ea945815b0a23dea4fa0c7
Chris Forbes <chrisforbes@google.com>
2017-07-06 18:19:35 -0700

    fix copyright test

--- source/small_vector.h
+++ source/small_vector.h
@@ -1,4 +1,19 @@
-#pragma once
+// Copyright (c) 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef LIBSPIRV_SMALL_VECTOR_H_
+#define LIBSPIRV_SMALL_VECTOR_H_
 
 #include <algorithm>
 #include <utility>
@@ -180,3 +195,5 @@ public:
   reference operator[](size_t index) { return *(begin() + index); }
   const_reference operator[](size_t index) const { return *(begin() + index); }
 };
+
+#endif  // LIBSPIRV_SMALL_VECTOR_H_
